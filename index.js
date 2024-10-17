c18081dfa9916a9665fb11fa0262547ee1dcfcb6
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-10-17 08:53:23 +0300

    Extract and display food categories from image URL

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ document.addEventListener("DOMContentLoaded", () => {
         fetch('https://foodish-api.com/api')
         .then(res => res.json())
         .then(data => {
+
+            const imgContainer = document.createElement('img')
+            imgContainer.style.margin = "10px"
             //Create an image element to specify the size and store an image
             const imgFood = document.createElement('img')
             imgFood.src = data.image
@@ -13,6 +16,17 @@ document.addEventListener("DOMContentLoaded", () => {
             imgFood.style.width = "200px"
             imgFood.style.height = "200px";
             foodMenu.appendChild(imgFood)
+
+            //Images being produced are varied 
+            //lets see whether we can use the image name as identifiers
+            //Extract category from image URL
+            const category = data.image.split('/')[4]
+            const categoryName = document.createElement('p')
+            categoryName.textContent = category.charAt(0).toUpperCase() + category.slice(1)
+
+            imgContainer.appendChild(imgFood)
+            imgContainer.appendChild(categoryName)
+            foodMenu.appendChild(imgContainer)
         })
 
     }
