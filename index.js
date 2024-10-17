2449f3a0cd6ddb33a3e82c63c88c8b9d8eaed4b0
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-10-17 08:35:27 +0300

    Introduce a for loop to generate 10 images

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ document.addEventListener("DOMContentLoaded", () => {
     const foodMenu = document.getElementById('food-menu')
 
     //Use our Fetch 
-    fetch('https://foodish-api.com/api')
-    .then(res => res.json())
-    .then(data => {
-        const imgFood = document.createElement('img')
-        imgFood.src = data.image
-        imgFood.alt = "Food Image"
-        imgFood.style.width = "200px"
-        imgFood.style.height = "200px";
-        foodMenu.appendChild(imgFood)
-    })
+    for (let i=0; i < 10; i++) {
+        fetch('https://foodish-api.com/api')
+        .then(res => res.json())
+        .then(data => {
+            //Create an image element to specify the size and store an image
+            const imgFood = document.createElement('img')
+            imgFood.src = data.image
+            imgFood.alt = "Food Image"
+            imgFood.style.width = "200px"
+            imgFood.style.height = "200px";
+            foodMenu.appendChild(imgFood)
+        })
+
+    }
+
 })
